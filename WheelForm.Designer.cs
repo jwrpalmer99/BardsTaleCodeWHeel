93d2e21a35ed170f88990d91b59070fc1fe35cd7
jwrpalmer99 <jeremyp@outlook.com>
2018-09-29 19:51:20 +0100

    Initial commit

---
+++ WheelForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BardsTaleCodeWHeel
+{
+    partial class WheelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WheelForm));
+            this.picA = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picA
+            // 
+            this.picA.BackColor = System.Drawing.Color.Transparent;
+            this.picA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picA.InitialImage = null;
+            this.picA.Location = new System.Drawing.Point(0, 0);
+            this.picA.Name = "picA";
+            this.picA.Size = new System.Drawing.Size(852, 853);
+            this.picA.TabIndex = 0;
+            this.picA.TabStop = false;
+            this.picA.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.picA.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picA_MouseDown);
+            this.picA.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picA_MouseMove);
+            this.picA.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picA_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Navy;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(703, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WheelForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(852, 853);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picA);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(852, 853);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(852, 853);
+            this.Name = "WheelForm";
+            this.Text = "Bards Tale IV Code Wheel";
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picA_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picA)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picA;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WheelForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BardsTaleCodeWHeel
+{
+    public partial class WheelForm : Form
+    {
+        Image wheelA;
+        Image wheelB;
+        Image wheelC;
+
+        public WheelForm()
+        {
+            InitializeComponent();
+            this.BackColor = Color.DarkBlue;
+            this.TransparencyKey = Color.DarkBlue;
+
+            wheelA = Bitmap.FromFile("wheelA.png");
+            wheelB = Bitmap.FromFile("wheelB.png");
+            wheelC = Bitmap.FromFile("wheelC.png");
+
+            this.picA.BackgroundImage = mergewheel(0, 0);
+        }
+
+        private bool mouseDown;
+        private Point lastLocation;
+        private double wheelCangle = 0;
+        private double wheelBangle = 0;
+
+        public float NearestRound(float x, float delX)
+        {
+            if (delX < 1)
+            {
+                float i = (float)Math.Floor(x);
+                float x2 = i;
+                while ((x2 += delX) < x) ;
+                float x1 = x2 - delX;
+                return (Math.Abs(x - x1) < Math.Abs(x - x2)) ? x1 : x2;
+            }
+            else
+            {
+                return (float)Math.Round(x / delX, MidpointRounding.AwayFromZero) * delX;
+            }
+        }
+
+        private Bitmap RotateImage(Bitmap bmp, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(bmp.Width, bmp.Height);
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                // Set the rotation point to the center in the matrix
+                g.TranslateTransform(bmp.Width / 2, bmp.Height / 2);
+                // Rotate
+                g.RotateTransform(angle);
+                // Restore rotation point in the matrix
+                g.TranslateTransform(-bmp.Width / 2, -bmp.Height / 2);
+                // Draw the image on the bitmap
+                g.DrawImage(bmp, new Point(0, 0));
+            }
+
+            return rotatedImage;
+        }
+
+        private Bitmap mergewheel(double angleC, double angleB)
+        {
+            Bitmap b = new Bitmap(wheelA);
+            Graphics g = Graphics.FromImage(b);
+
+            //add B
+            Bitmap b_rotate = new Bitmap(wheelB);
+            g.DrawImageUnscaledAndClipped(RotateImage(b_rotate, (float)angleB), new Rectangle(0,0,b.Width,b.Height));
+
+            //add C
+            Bitmap c_rotate = new Bitmap(wheelC);
+            g.DrawImageUnscaledAndClipped(RotateImage(c_rotate, (float)angleC), new Rectangle(0, 0, b.Width, b.Height));
+
+            g.Dispose();
+
+            return b;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs m = (MouseEventArgs)e;
+
+            if (m.X > 791 && m.X < 838 && m.Y > 19 && m.Y < 63)
+                this.Close();
+
+            double mx = m.X - (picA.Width / 2);
+            double my = m.Y - (picA.Height / 2);
+
+            double radius = Math.Sqrt((mx * mx) + (my *my));
+            double angle = GetAngleFromPoint(mx,my);
+
+            Debug.WriteLine("angle = " + angle + ", radius = " + radius);
+
+            if (radius > 299 && radius < 331)
+            {
+                //wheelC angle
+                wheelCangle = NearestRound((float)(angle - 7), 15);
+                this.picA.BackgroundImage = mergewheel(wheelBangle, wheelCangle);
+                return;
+            }
+            if (radius > 330 && radius < 424)
+            {
+                //wheelB angle
+                wheelBangle = NearestRound((float)(angle - 15), 30);
+                this.picA.BackgroundImage = mergewheel(wheelBangle, wheelCangle);
+                return;
+            }
+        }
+
+        internal double GetAngleFromPoint(double dx, double dy)
+        {
+            var theta = Math.Atan2(dx, -dy);
+            var angle = ((theta * 180 / Math.PI) + 360) % 360;
+            return angle;
+        }
+
+        private void picA_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+        
+        private void picA_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void picA_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
